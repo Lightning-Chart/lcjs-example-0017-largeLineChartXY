71eb9c04cba3f0452b50fe5efbb3a905ab861057
Ilkka Kuosmanen <ilkka.kuosmanen@lightningchart.com>
2024-02-22 13:55:47 +0200

    Update

--- src/index.js
+++ src/index.js
@@ -8,9 +8,9 @@ const lcjs = require('@arction/lcjs')
 const xydata = require('@arction/xydata')
 
 // Extract required parts from LightningChartJS.
-const { lightningChart, PointShape, ColorCSS, SolidLine, SolidFill, Themes } = lcjs
+const { lightningChart, emptyFill, Themes } = lcjs
 
-const { createProgressiveTraceGenerator } = require('@arction/xydata')
+const { createProgressiveTraceGenerator } = xydata
 
 const trendsCount = 10
 const dataPerTrend = 500 * 1000
@@ -22,31 +22,24 @@ const chart = lightningChart()
     })
     .setTitle(`Line Chart with large data set (${((trendsCount * dataPerTrend) / 10 ** 6).toFixed(1)} million data points)`)
 
-const seriesArray = new Array(trendsCount).fill().map((_) =>
-    chart
-        .addLineSeries({
-            dataPattern: {
-                pattern: 'ProgressiveX',
-            },
-        })
-        .setStrokeStyle((stroke) => stroke.setThickness(1)),
-)
-
-seriesArray.forEach((series) => {
-    createProgressiveTraceGenerator()
-        .setNumberOfPoints(dataPerTrend)
-        .generate()
-        .toPromise()
-        .then((data) => {
-            series.add(data)
+Promise.all(
+    new Array(trendsCount).fill(0).map((_) => createProgressiveTraceGenerator().setNumberOfPoints(dataPerTrend).generate().toPromise()),
+).then((allData) => {
+    for (let i = 0; i < trendsCount; i += 1) {
+        chart
+            .addPointLineAreaSeries({
+                dataPattern: 'ProgressiveX',
+            })
+            .setStrokeStyle((stroke) => stroke.setThickness(1))
+            .setAreaFillStyle(emptyFill)
+            .appendJSON(allData[i])
+    }
+    const legend = chart
+        .addLegendBox()
+        .add(chart)
+        // Dispose example UI elements automatically if they take too much space. This is to avoid bad UI on mobile / etc. devices.
+        .setAutoDispose({
+            type: 'max-width',
+            maxWidth: 0.3,
         })
 })
-
-const legend = chart
-    .addLegendBox()
-    .add(chart)
-    // Dispose example UI elements automatically if they take too much space. This is to avoid bad UI on mobile / etc. devices.
-    .setAutoDispose({
-        type: 'max-width',
-        maxWidth: 0.3,
-    })
